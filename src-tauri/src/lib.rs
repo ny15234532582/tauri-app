bd7247c6cb5659a4849fde9067ed5ba514010b90
ny15234532582 <47020822+ny15234532582@users.noreply.github.com>
2026-01-25 16:45:43 +0800

    开始登录完成

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,27 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Response {
+  status: u32,
+  data: String,
+}
+
+//用户登录
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn login(username: String, password: String) -> Response {
+  println!("开始登录 username = {username}; password = {password}");
+  if username == "admin" && password == "admin123" {
+    Response{ status: 200, data: "登录成功".to_string() }
+  } else {
+    Response{ status: 500, data: "账号或密码错误".to_string() }
+  }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .plugin(tauri_plugin_opener::init())
+    .invoke_handler(tauri::generate_handler![login])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
